82da29ae2c1b8ffaba88965406d06ea85d42cdab
Aidan <aidankmcalister@gmail.com>
2024-02-01 15:40:18 -0600

    backgropund change

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import MainContainer from "./components/MainContainer";
 
 function App() {
   return (
-    <div className="bg-[url('./assets/bg_img.jpg')] absolute w-screen h-screen overflow-scroll bg-right sm:font-ubuntu md:font-roboto lg:font-roboto">
-      <div className="bg-cover h-screen w-screen backdrop-blur-lg bg-white/30 overflow-scroll">
+    // <div className="bg-[url('./assets/bg_img.jpg')] absolute w-screen h-screen overflow-scroll bg-right sm:font-ubuntu md:font-roboto lg:font-roboto">
+    <div className="absolute w-screen h-screen overflow-scroll sm:font-ubuntu md:font-roboto lg:font-roboto">
+      <div className="bg-cover h-screen w-screen backdrop-blur-lg bg-white/40 overflow-scroll">
         <div className="flex flex-col items-center grow-0">
           <MainContainer />
         </div>
